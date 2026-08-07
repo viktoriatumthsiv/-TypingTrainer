21c623e9fab370544a04198af485881d072d8f07
Viktoria Tumthsiv <viktoriatumthisv@gmail.com>
2026-08-07 11:19:57 +0300

    Modify Vite config for React plugin and base path
    Updated the import statement for the React plugin and added a base path for the repository.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/-TypingTrainer/', // Назва вашого репозиторію (з дефісом)
 })
