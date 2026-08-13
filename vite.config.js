ba86cc124d56df83927fdded813e3581ec7bd783
Viktoria Tumthsiv <viktoriatumthisv@gmail.com>
2026-08-13 12:16:15 +0300

    Fix vite config for Render

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/-TypingTrainer/', // Назва вашого репозиторію (з дефісом)
+  base: '/',
 })
